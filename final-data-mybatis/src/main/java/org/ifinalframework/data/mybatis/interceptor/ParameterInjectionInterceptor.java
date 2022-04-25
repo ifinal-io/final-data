9e0816ba13f94b441470cbac83385f19b01666df
ilikly <ifinal.io@qq.com>
2022-04-25 14:45:13 +0800

    add mybatis-plus tenant interceptor

--- final-data-mybatis/src/main/java/org/ifinalframework/data/mybatis/interceptor/ParameterInjectionInterceptor.java
+++ final-data-mybatis/src/main/java/org/ifinalframework/data/mybatis/interceptor/ParameterInjectionInterceptor.java
@@ -15,39 +15,30 @@
 
 package org.ifinalframework.data.mybatis.interceptor;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.Map;
+
 import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.defaults.DefaultSqlSession;
 
-import org.ifinalframework.context.user.UserContextHolder;
-import org.ifinalframework.core.IEntity;
-import org.ifinalframework.data.mybatis.mapper.AbsMapper;
-import org.ifinalframework.data.query.DefaultQEntityFactory;
-import org.ifinalframework.data.repository.Repository;
-import org.ifinalframework.query.QEntity;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.core.ResolvableType;
 import org.springframework.core.annotation.Order;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.util.Map;
-import java.util.Properties;
+import org.ifinalframework.context.user.UserContextHolder;
+import org.ifinalframework.data.mybatis.mapper.AbsMapper;
+import org.ifinalframework.data.query.DefaultQEntityFactory;
+import org.ifinalframework.query.QEntity;
 
 /**
  * 参数注入拦截器
@@ -69,66 +60,45 @@ import java.util.Properties;
 @Order
 @Component
 @SuppressWarnings({"unchecked", "rawtypes"})
-public class ParameterInjectionInterceptor implements Interceptor {
-
-    private static final Logger logger = LoggerFactory.getLogger(ParameterInjectionInterceptor.class);
+public class ParameterInjectionInterceptor extends AbsMapperInterceptor {
 
     private static final String TABLE_PARAMETER_NAME = "table";
 
     private static final String PROPERTIES_PARAMETER_NAME = "properties";
 
-    public static <I extends Serializable, T extends IEntity<I>> Class<T> from(
-            final @NonNull Class<? extends AbsMapper> mapper) {
-
-        return (Class<T>) ResolvableType.forClass(mapper)
-                .as(AbsMapper.class)
-                .resolveGeneric(1);
-
-    }
 
     @Override
-    public Object intercept(final Invocation invocation) throws Throwable {
+    protected Object intercept(Invocation invocation, Class<?> mapper, Class<?> entityClass) throws Throwable {
 
         Object[] args = invocation.getArgs();
-        MappedStatement ms = (MappedStatement) args[0];
-        final String id = ms.getId();
-        logger.debug("Inject Parameter start...:{}", id);
-        try {
 
-            final String mapperName = id.substring(0, id.lastIndexOf("."));
-            final Class<?> mapper = Class.forName(mapperName);
-            Object parameter = args[1];
+        Object parameter = args[1];
 
-            if (parameter instanceof Map && AbsMapper.class.isAssignableFrom(mapper)) {
-                Map<String, Object> parameters = (Map<String, Object>) parameter;
+        if (parameter instanceof Map && AbsMapper.class.isAssignableFrom(mapper)) {
+            Map<String, Object> parameters = (Map<String, Object>) parameter;
 
-                final Class<IEntity<Serializable>> entityClass = from((Class<? extends AbsMapper>) mapper);
-                final QEntity<?, ?> entity = DefaultQEntityFactory.INSTANCE.create(entityClass);
-                parameters.computeIfAbsent(TABLE_PARAMETER_NAME, k -> entity.getTable());
-                parameters.putIfAbsent(PROPERTIES_PARAMETER_NAME, entity);
-                parameters.putIfAbsent("USER", UserContextHolder.getUser());
+            final QEntity<?, ?> entity = DefaultQEntityFactory.INSTANCE.create(entityClass);
+            parameters.computeIfAbsent(TABLE_PARAMETER_NAME, k -> entity.getTable());
+            parameters.putIfAbsent(PROPERTIES_PARAMETER_NAME, entity);
+            parameters.putIfAbsent("USER", UserContextHolder.getUser());
 
-                /**
-                 * {@link org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator} only supports {@link Map} types:
-                 * <ul>
-                 *     <li>{@link org.apache.ibatis.binding.MapperMethod.ParamMap}</li>
-                 *     <li>{@link DefaultSqlSession.StrictMap}</li>
-                 * </ul>
-                 * @see org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator#assignKeys(Configuration, ResultSet, ResultSetMetaData, String[], Object)
-                 * @since 1.2.2
-                 */
-                final MapperMethod.ParamMap<Object> paramMap = new MapperMethod.ParamMap<>();
-                paramMap.putAll((Map<? extends String, ?>) parameter);
-                args[1] = paramMap;
+            /**
+             * {@link org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator} only supports {@link Map} types:
+             * <ul>
+             *     <li>{@link org.apache.ibatis.binding.MapperMethod.ParamMap}</li>
+             *     <li>{@link DefaultSqlSession.StrictMap}</li>
+             * </ul>
+             * @see org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator#assignKeys(Configuration, ResultSet, ResultSetMetaData, String[], Object)
+             * @since 1.2.2
+             */
+            final MapperMethod.ParamMap<Object> paramMap = new MapperMethod.ParamMap<>();
+            paramMap.putAll((Map<? extends String, ?>) parameter);
+            args[1] = paramMap;
 
-            }
-
-
-            return invocation.proceed();
-
-        } finally {
-            logger.debug("Inject Parameter Finish...:{}", id);
         }
+
+        return invocation.proceed();
+
 
     }
 
