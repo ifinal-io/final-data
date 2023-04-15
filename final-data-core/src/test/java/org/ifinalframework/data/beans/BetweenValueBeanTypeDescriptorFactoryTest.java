b1ff1ee41d3682e722258d80a7af0bee24472b81
ilikly <ifinal.io@qq.com>
2023-04-15 11:18:22 +0800

    update BetweenValueBeanTypeDescriptorFactoryTest

--- final-data-core/src/test/java/org/ifinalframework/data/beans/BetweenValueBeanTypeDescriptorFactoryTest.java
+++ final-data-core/src/test/java/org/ifinalframework/data/beans/BetweenValueBeanTypeDescriptorFactoryTest.java
@@ -15,11 +15,16 @@
 
 package org.ifinalframework.data.beans;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.springframework.core.ResolvableType;
 import org.springframework.core.convert.TypeDescriptor;
 
 import org.ifinalframework.data.query.BetweenValue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,7 +42,53 @@ class BetweenValueBeanTypeDescriptorFactoryTest {
     void create() {
         final ResolvableType resolvableType = ResolvableType.forClassWithGenerics(BetweenValue.class, Integer.class);
         final BetweenValue betweenValue = beanTypeDescriptorFactory.create(Integer.class, new TypeDescriptor(resolvableType, null, null));
-        betweenValue.setMax(1);
-        betweenValue.setMin(2);
+        betweenValue.setMin(1);
+        betweenValue.setMax(2);
+        Assertions.assertEquals(1,betweenValue.getMin());
+        Assertions.assertEquals(2,betweenValue.getMax());
     }
+
+    @Test
+    void longBetweenValue(){
+        final ResolvableType resolvableType = ResolvableType.forClassWithGenerics(BetweenValue.class, Long.class);
+        final BetweenValue betweenValue = beanTypeDescriptorFactory.create(Long.class, new TypeDescriptor(resolvableType, null, null));
+        betweenValue.setMin(1L);
+        betweenValue.setMax(2L);
+        Assertions.assertEquals(1L,betweenValue.getMin());
+        Assertions.assertEquals(2L,betweenValue.getMax());
+    }
+
+    @Test
+    void localDateTimeBetweenValue(){
+        final ResolvableType resolvableType = ResolvableType.forClassWithGenerics(BetweenValue.class, LocalDateTime.class);
+        final BetweenValue betweenValue = beanTypeDescriptorFactory.create(LocalDateTime.class, new TypeDescriptor(resolvableType, null, null));
+
+        betweenValue.setMin(LocalDate.now().atStartOfDay());
+        betweenValue.setMax(LocalDate.now().atTime(23,59,59));
+        Assertions.assertEquals(LocalDate.now().atStartOfDay(),betweenValue.getMin());
+        Assertions.assertEquals(LocalDate.now().atTime(23,59,59),betweenValue.getMax());
+    }
+
+    @Test
+    void localDateBetweenValue(){
+        final ResolvableType resolvableType = ResolvableType.forClassWithGenerics(BetweenValue.class, LocalDate.class);
+        final BetweenValue betweenValue = beanTypeDescriptorFactory.create(LocalDate.class, new TypeDescriptor(resolvableType, null, null));
+
+        betweenValue.setMin(LocalDate.now());
+        betweenValue.setMax(LocalDate.now());
+        Assertions.assertEquals(LocalDate.now(),betweenValue.getMin());
+        Assertions.assertEquals(LocalDate.now(),betweenValue.getMax());
+    }
+
+    @Test
+    void dateBetweenValue(){
+        final ResolvableType resolvableType = ResolvableType.forClassWithGenerics(BetweenValue.class, Date.class);
+        final BetweenValue betweenValue = beanTypeDescriptorFactory.create(Date.class, new TypeDescriptor(resolvableType, null, null));
+
+        betweenValue.setMin(new Date(LocalDate.now().toEpochDay()));
+        betweenValue.setMax(new Date(LocalDate.now().toEpochDay()));
+        Assertions.assertEquals(new Date(LocalDate.now().toEpochDay()),betweenValue.getMin());
+        Assertions.assertEquals(new Date(LocalDate.now().toEpochDay()),betweenValue.getMax());
+    }
+
 }
